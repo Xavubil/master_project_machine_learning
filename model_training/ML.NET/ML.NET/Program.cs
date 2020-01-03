3c45706e40973b15b2f7338758545abf4df37817
Micha Beierl <micha.beierl@gmail.com>
2020-01-03 10:52:51 +0100

    train first SGD-Model Adding Model-evaluation

--- model_training/ML.NET/ML.NET/Program.cs
+++ model_training/ML.NET/ML.NET/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using Microsoft.ML;//Benötigt NuGet-Package Microsoft.ML!
@@ -20,9 +21,16 @@ namespace ML.NET
 
     class Program
     {
-        public static List<DataFormat> loadFile(string path)
+        public static void loadFile(string path, out List<DataFormat> training, out List<DataFormat> evaluation, out float positiveClassWeight)
         {
-            List<DataFormat> returner = new List<DataFormat>();
+            float countPositive = 0;
+            float countNegative = 0;
+
+            training = new List<DataFormat>();
+            evaluation = new List<DataFormat>();
+
+            List<DataFormat> pos = new List<DataFormat>();
+            List<DataFormat> neg = new List<DataFormat>();
 
             StreamReader sr = new StreamReader(path);
             string line = sr.ReadLine();
@@ -38,38 +46,115 @@ namespace ML.NET
                 }
                 df.featureVector = featureVector;
                 df.label = (splitted[14] == "i.O.");
-                returner.Add(df);
+                if(df.label)
+                    pos.Add(df);
+                else
+                    neg.Add(df);
             }
             sr.Close();
-            return returner;
-        }
 
+            Random random = new Random(1);
+            foreach(DataFormat item in pos)
+            {
+                if(random.NextDouble() < 0.8)
+                {
+                    training.Add(item);
+                    countPositive++;
+                }
+                else
+                {
+                    evaluation.Add(item);
+                }
+            }
+            foreach(DataFormat item in neg)
+            {
+                if(random.NextDouble() < 0.8)
+                {
+                    training.Add(item);
+                    countNegative++;
+                }
+                else
+                {
+                    evaluation.Add(item);
+                }
+            }
+            positiveClassWeight = countNegative / countPositive;
+        }
+        
         public static string currentDir = Path.Combine(Environment.CurrentDirectory, "../../../");
         public static string cacheDir = Path.Combine(currentDir, "cacheDir");
         public static string pathTrainData = Path.Combine(currentDir, "trainingdata.csv");
         public static string logPath = Path.Combine(cacheDir, "log.txt");
+        public static string evalDatasetPath = Path.Combine(cacheDir, "evalData.txt");
 
         static void Main(string[] args)
         {
             MLContext context = new MLContext();
-            var data = loadFile(pathTrainData);
-            //TODO seperate training- and evaluation-dataset classwise and (80/20)
-            IDataView traindata = context.Data.LoadFromEnumerable(data);
-            
+            System.IO.Directory.CreateDirectory(cacheDir);
+            StreamWriter sw = new StreamWriter(logPath);
+
+            List<DataFormat> training, evaluation;
+            float positiveWeight;
+            loadFile(pathTrainData,out training,out evaluation, out positiveWeight);
+
+            sw.WriteLine("Size training: " + training.Count);
+            sw.WriteLine("Size evluation: " + evaluation.Count);
+            sw.WriteLine("PositiveWeight: " + positiveWeight);
+
+            IDataView traindata = context.Data.LoadFromEnumerable(training);
+            IDataView evalData = context.Data.LoadFromEnumerable(evaluation);
 
             var options = new SgdCalibratedTrainer.Options()
             {
                 LabelColumnName = "Label",
                 FeatureColumnName = "Feature",
-                NumberOfIterations = 30,
-                LearningRate = 0.0001
-                //TODO weight inputs, based on class (unbalanced dataset)
+                NumberOfIterations = 50,
+                LearningRate = 0.01,
+                PositiveInstanceWeight = positiveWeight
             };
 
             var pipeline = context.BinaryClassification.Trainers.SgdCalibrated(options);
 
-            var model = pipeline.Fit();//TODO fit
-            //TODO add evaluation
+            Console.WriteLine("Start training...");
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var model = pipeline.Fit(traindata);
+            watch.Stop();
+            Console.WriteLine("finished training. Time: " + watch.Elapsed.ToString());
+            sw.WriteLine("Elapsed time: " + watch.Elapsed.ToString());
+            sw.WriteLine();
+            int i = 0;
+            foreach(var weight in model.Model.SubModel.Weights)
+            {
+                sw.WriteLine("weight " + i + ": " + weight);
+            }
+
+            IDataView transformedEval = model.Transform(evalData);
+            /*var cursor = transformedEval.GetRowCursor(transformedEval.Schema);
+            cursor.MoveNext();
+            StreamWriter swEvalData = new StreamWriter(evalDatasetPath);
+            foreach(var col in cursor.Schema)
+            {
+                swEvalData.Write(col.Name + "\t");
+            }
+            swEvalData.WriteLine();
+            do
+            {
+                swEvalData.WriteLine(cursor.ToString());
+            } while (cursor.MoveNext());
+            swEvalData.Close();*/
+            var evalMetrics = context.BinaryClassification.Evaluate(transformedEval);
+
+            sw.WriteLine();
+            sw.WriteLine(evalMetrics.ConfusionMatrix.GetFormattedConfusionTable());
+            sw.WriteLine();
+
+            Console.WriteLine("Accuracy: " + evalMetrics.Accuracy);
+            sw.WriteLine("Accuracy: " + evalMetrics.Accuracy);
+            Console.WriteLine("LogLoss: " + evalMetrics.LogLoss);
+            sw.WriteLine("Logloss: " + evalMetrics.LogLoss);
+            sw.Close();
+            Console.ReadLine();
         }
     }
 }
