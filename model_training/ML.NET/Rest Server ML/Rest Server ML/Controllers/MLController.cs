db7dca7b56e3be53189670503ec1d83da435f4c6
stbehirs <guthix102@googlemail.com>
2020-01-04 08:00:17 +0100

    ML.NET Rest Server hinzugefügt (Grundgerüst)

---
+++ model_training/ML.NET/Rest Server ML/Rest Server ML/Controllers/MLController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rest_Server_ML;
+
+namespace Rest_Server_ML.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MLController : ControllerBase
+    {
+        // GET: api/ML
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ML/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            MLClass.runML();
+
+            return "true";
+        }
+
+        // POST: api/ML
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ML/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
---
+++ model_training/ML.NET/Rest Server ML/Rest Server ML/MLClass.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using Microsoft.ML;//Benötigt NuGet-Package Microsoft.ML!
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+
+namespace Rest_Server_ML
+{
+    public class MLClass
+    {
+        public static string currentDir = Path.Combine(Environment.CurrentDirectory, "");
+        public static string cacheDir = Path.Combine(currentDir, "cacheDir");
+        public static string pathTrainData = Path.Combine(currentDir, "trainingdata.csv");
+        public static string logPath = Path.Combine(cacheDir, "log.txt");
+        public static string evalDatasetPath = Path.Combine(cacheDir, "evalData.txt");
+
+        public static void runML()
+        {
+            MLContext context = new MLContext();
+            System.IO.Directory.CreateDirectory(cacheDir);
+            StreamWriter sw = new StreamWriter(logPath);
+
+            List<DataFormat> training, evaluation;
+            float positiveWeight;
+            loadFile(pathTrainData, out training, out evaluation, out positiveWeight);
+
+            sw.WriteLine("Size training: " + training.Count);
+            sw.WriteLine("Size evluation: " + evaluation.Count);
+            sw.WriteLine("PositiveWeight: " + positiveWeight);
+
+            IDataView traindata = context.Data.LoadFromEnumerable(training);
+            IDataView evalData = context.Data.LoadFromEnumerable(evaluation);
+
+            var options = new SgdCalibratedTrainer.Options()
+            {
+                LabelColumnName = "Label",
+                FeatureColumnName = "Feature",
+                NumberOfIterations = 50,
+                LearningRate = 0.01,
+                PositiveInstanceWeight = positiveWeight
+            };
+
+            var pipeline = context.BinaryClassification.Trainers.SgdCalibrated(options);
+
+            Console.WriteLine("Start training...");
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var model = pipeline.Fit(traindata);
+            watch.Stop();
+            Console.WriteLine("finished training. Time: " + watch.Elapsed.ToString());
+            sw.WriteLine("Elapsed time: " + watch.Elapsed.ToString());
+            sw.WriteLine();
+            int i = 0;
+            foreach (var weight in model.Model.SubModel.Weights)
+            {
+                sw.WriteLine("weight " + i + ": " + weight);
+            }
+
+            IDataView transformedEval = model.Transform(evalData);
+            /*var cursor = transformedEval.GetRowCursor(transformedEval.Schema);
+            cursor.MoveNext();
+            StreamWriter swEvalData = new StreamWriter(evalDatasetPath);
+            foreach(var col in cursor.Schema)
+            {
+                swEvalData.Write(col.Name + "\t");
+            }
+            swEvalData.WriteLine();
+            do
+            {
+                swEvalData.WriteLine(cursor.ToString());
+            } while (cursor.MoveNext());
+            swEvalData.Close();*/
+            var evalMetrics = context.BinaryClassification.Evaluate(transformedEval);
+
+            sw.WriteLine();
+            sw.WriteLine(evalMetrics.ConfusionMatrix.GetFormattedConfusionTable());
+            sw.WriteLine();
+
+            Console.WriteLine("Accuracy: " + evalMetrics.Accuracy);
+            sw.WriteLine("Accuracy: " + evalMetrics.Accuracy);
+            Console.WriteLine("LogLoss: " + evalMetrics.LogLoss);
+            sw.WriteLine("Logloss: " + evalMetrics.LogLoss);
+            sw.Close();
+            //Console.ReadLine();
+        }
+        public static void loadFile(string path, out List<DataFormat> training, out List<DataFormat> evaluation, out float positiveClassWeight)
+        {
+            float countPositive = 0;
+            float countNegative = 0;
+
+            training = new List<DataFormat>();
+            evaluation = new List<DataFormat>();
+
+            List<DataFormat> pos = new List<DataFormat>();
+            List<DataFormat> neg = new List<DataFormat>();
+
+            StreamReader sr = new StreamReader(path);
+            string line = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                string[] splitted = line.Split(',');
+                DataFormat df = new DataFormat();
+                float[] featureVector = new float[12];
+                for (int i = 0; i < 12; i++)
+                {
+                    float.TryParse(splitted[i], System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out featureVector[i]);
+                }
+                df.featureVector = featureVector;
+                df.label = (splitted[14] == "i.O.");
+                if (df.label)
+                    pos.Add(df);
+                else
+                    neg.Add(df);
+            }
+            sr.Close();
+
+            Random random = new Random(1);
+            foreach (DataFormat item in pos)
+            {
+                if (random.NextDouble() < 0.8)
+                {
+                    training.Add(item);
+                    countPositive++;
+                }
+                else
+                {
+                    evaluation.Add(item);
+                }
+            }
+            foreach (DataFormat item in neg)
+            {
+                if (random.NextDouble() < 0.8)
+                {
+                    training.Add(item);
+                    countNegative++;
+                }
+                else
+                {
+                    evaluation.Add(item);
+                }
+            }
+            positiveClassWeight = countNegative / countPositive;
+        }
+    }
+
+    public class DataFormat
+    {
+        [VectorType(12)]
+        [ColumnName("Feature")]
+        public float[] featureVector { get; set; }
+
+        [ColumnName("Label")]
+        public bool label { get; set; }
+    }
+}
